b3f2ca6afe14fb97a0592cd4c4107ba08bed0846
Simon Babovic <simon@babovic.at>
2024-12-15 18:22:45 +0100

    add flake.nix

--- flake.nix
+++ flake.nix
@@ -1,22 +1,38 @@
 {
   description = "qudit-compression";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
+      lib = nixpkgs.lib;
+      pythonldlibpath = lib.makeLibraryPath (with pkgs; [
+        zlib
+        zstd
+        stdenv.cc.cc
+        curl
+        openssl
+        attr
+        libssh
+        bzip2
+        libxml2
+        acl
+        libsodium
+        util-linux
+        xz
+        systemd
+      ]);
     in {
       devShell = pkgs.mkShell {
         nativeBuildInputs = [ pkgs.bashInteractive ];
         buildInputs = with pkgs; [
-          (python311.withPackages (python-pkgs: with python-pkgs; [
-            pip
-            # matplotlib
-            # pylatexenc
-          ]))
+          python311
         ];
         shellHook = with pkgs; ''
+          ${python311}/bin/python -m venv ./.virtualenv
+          source ./.virtualenv/bin/activate
+          export LD_LIBRARY_PATH="${pythonldlibpath}"
           pip install -r requirements.txt
         '';
       };
